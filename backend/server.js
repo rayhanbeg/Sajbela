5dea405b58e757584839ba687cc3db2c406a441c
rayhanbeg <mdrayhan40301@gmail.com>
2025-06-24 16:07:28 +0600

    cors  related issue

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const app = express()
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5000",
+    origin: process.env.FRONTEND_URL || ["http://localhost:5173", "https://sajbelafrontend.vercel.app"],
     credentials: true,
   })
 )
