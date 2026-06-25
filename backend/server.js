3758354f8278c146da9b7848ab414d6027eefa75
Md Rayhan <mdrayhan40301@gmail.com>
2026-06-25 20:22:38 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const app = express()
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || ["http://localhost:5173", "https://www.sajbela.shop"],
+    origin: process.env.FRONTEND_URL || ["http://localhost:5173", "https://sajbela.arvit.site"],
     credentials: true,
   })
 )
